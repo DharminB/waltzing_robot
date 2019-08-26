fa6a7ed963459467dddfdcd85875a82294cfef7d
Dharmin <dharmingeo@gmail.com>
2019-08-26 23:38:45 +0200

    Improved design + skeleton for spline curve motion

--- ros/src/waltzing_robot/vel_curve_handler.py
+++ ros/src/waltzing_robot/vel_curve_handler.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import math
 from waltzing_robot.utils import Utils
+from waltzing_robot.waypoints import Waypoint, Waypoints
 
 class VelCurveHandler(object):
 
@@ -16,90 +17,118 @@ class VelCurveHandler(object):
     def __init__(self, **kwargs):
         self.max_vel = kwargs.get('max_vel', float('inf'))
         self.max_acc = kwargs.get('max_acc', float('inf'))
-        self.x, self.y, self.theta = None, None, None
-        self.time, self.vel_curve = None, "default"
-        self.curve_specific_data = dict()
+        self.trajectory_data_list = list()
+        self.trajectory_index = 0
 
     def __str__(self):
         string = ""
         string += 'max_vel: ' + str(self.max_vel) + '\n'
         string += 'max_acc: ' + str(self.max_acc) + '\n'
-        string += 'delta_x: ' + str(self.x) + '\n'
-        string += 'delta_y: ' + str(self.y) + '\n'
-        string += 'delta_theta: ' + str(self.theta) + '\n'
-        string += 'delta_time: ' + str(self.time) + '\n'
-        string += 'vel_curve: ' + str(self.vel_curve) + '\n'
-        string += 'curve_data: ' + str(self.curve_specific_data)
+        string += 'trajectory_data_list: ' + str(self.trajectory_data_list)
         return string
 
-    def set_delta(self, delta_dict):
-        """set the delta dict containing all neccessary info for trajectory generation
+    def set_params(self, waypoints):
+        """Calculates the neccessary values needed for all trajectories defined 
+        by set of waypoints. Returns if the list of trajectory is possible or not.
 
-        :delta_dict: dict
-        :returns: None
+        :waypoints: list of Waypoint objects
+        :returns: bool
 
         """
-        self.x = delta_dict['x']
-        self.y = delta_dict['y']
-        self.theta = delta_dict['theta']
-        self.time = delta_dict['time']
-        self.vel_curve = delta_dict['vel_curve']
-        if not (hasattr(self, self.vel_curve+"_vel") and callable(getattr(self, self.vel_curve+"_vel"))):
-            print("Invalid vel curve. Resetting to default")
-            self.vel_curve = "default"
-            return False
-        if hasattr(self, self.vel_curve+"_calc") and callable(getattr(self, self.vel_curve+"_calc")):
-            return getattr(self, self.vel_curve+"_calc")()
-        return False
+        # sanity check
+        for wp in waypoints:
+            assert isinstance(wp, Waypoint)
+
+        # calculate trajectory data
+        self.trajectory_data_list = list()
+        for i in range(len(waypoints)-1):
+            start_wp = waypoints[i]
+            end_wp = waypoints[i+1]
+            vel_curve = end_wp.vel_curve
+            if not (hasattr(self, vel_curve+"_vel") and \
+                    callable(getattr(self, vel_curve+"_vel")) and \
+                    hasattr(self, vel_curve+"_calc") and \
+                    callable(getattr(self, vel_curve+"_calc"))):
+                print("Invalid vel curve")
+                return False
+            curve_specific_data = getattr(self, vel_curve+"_calc")(start_wp, end_wp)
+            if curve_specific_data is None: # impossible trajectory
+                return False
+            self.trajectory_data_list.append(curve_specific_data)
+        return True
 
     def get_vel(self, time_duration, **kwargs):
-        return getattr(self, self.vel_curve+"_vel")(time_duration, **kwargs)
+        if len(self.trajectory_data_list) > self.trajectory_index:
+            vel_curve = self.trajectory_data_list[self.trajectory_index]['vel_curve']
+            return getattr(self, vel_curve+"_vel")(time_duration, **kwargs)
+        else:
+            return self.default_vel(time_duration, **kwargs)
 
     def default_vel(self, time_duration, current_position=(0.0, 0.0, 0.0)):
         return (0.0, 0.0, 0.0)
 
-    def linear_calc(self):
-        self.curve_specific_data['vel'] = Utils.get_distance(self.x, self.y)/self.time
-        return True
+    def linear_calc(self, start_wp, end_wp):
+        data = dict()
+        data['vel_curve'] = end_wp.vel_curve
+        data['theta'] = Utils.get_shortest_angle(end_wp.theta, start_wp.theta)
+        data['time'] = end_wp.time - start_wp.time
+        if end_wp.control_points is None:
+            data['x'] = end_wp.x - start_wp.x
+            data['y'] = end_wp.y - start_wp.y
+            data['vel'] = Utils.get_distance(data['x'], data['y'])/data['time']
+        else:
+            return None
+        return data
 
     def linear_vel(self, time_duration, current_position=(0.0, 0.0, 0.0)):
-        if time_duration >= self.time:
-            self.vel_curve = "default"
-            return (0.0, 0.0, 0.0)
-        omega = Utils.get_shortest_angle(math.atan2(self.y, self.x), current_position[2])
-        vel = self.curve_specific_data['vel']
-        return (vel * math.cos(omega), vel * math.sin(omega), self.theta/self.time)
+        data = self.trajectory_data_list[self.trajectory_index]
+        if time_duration >= data['time']:
+            return self.default_vel(time_duration, current_position)
+        omega = Utils.get_shortest_angle(math.atan2(data['y'], data['x']),
+                                         current_position[2])
+        return (data['vel'] * math.cos(omega),
+                data['vel'] * math.sin(omega),
+                data['theta']/data['time'])
 
-    def trapezoid_calc(self):
-        dist = Utils.get_distance(self.x, self.y)
-        discriminant = (self.time * self.max_acc)**2 - (4 * self.max_acc * dist)
-        if discriminant < 0:
-            print("No velocity solution found")
-            return False
-        des_vel_sol_1 = ((self.time * self.max_acc) + (discriminant)**0.5)/2.0
-        des_vel_sol_2 = ((self.time * self.max_acc) - (discriminant)**0.5)/2.0
-        if des_vel_sol_1 <= self.max_vel:
-            self.curve_specific_data['vel'] = des_vel_sol_1
-        elif des_vel_sol_2 <= self.max_vel:
-            self.curve_specific_data['vel'] = des_vel_sol_2
+    def trapezoid_calc(self, start_wp, end_wp):
+        data = dict()
+        data['vel_curve'] = end_wp.vel_curve
+        data['theta'] = Utils.get_shortest_angle(end_wp.theta, start_wp.theta)
+        data['time'] = end_wp.time - start_wp.time
+        if end_wp.control_points is None:
+            data['x'] = end_wp.x - start_wp.x
+            data['y'] = end_wp.y - start_wp.y
+            dist = Utils.get_distance(data['x'], data['y'])
+            discriminant = (data['time'] * self.max_acc)**2 - (4 * self.max_acc * dist)
+            if discriminant < 0:
+                print("No velocity solution found")
+                return None
+            des_vel_sol_1 = ((data['time'] * self.max_acc) + (discriminant)**0.5)/2.0
+            des_vel_sol_2 = ((data['time'] * self.max_acc) - (discriminant)**0.5)/2.0
+            if des_vel_sol_1 <= self.max_vel:
+                data['vel'] = des_vel_sol_1
+            elif des_vel_sol_2 <= self.max_vel:
+                data['vel'] = des_vel_sol_2
+            else:
+                print("No valid velocity solution found. Found solutions: ", des_vel_sol_1, "and ", des_vel_sol_2)
+                return None
+            data['acc_time'] = data['vel']/self.max_acc
+            data['const_vel_time'] = data['time'] - (2 * data['acc_time'])
         else:
-            print("No valid velocity solution found. Found solutions: ", des_vel_sol_1, "and ", des_vel_sol_2)
-            return False
-        self.curve_specific_data['acc_time'] = self.curve_specific_data['vel']/self.max_acc
-        self.curve_specific_data['const_vel_time'] = self.time - (2 * self.curve_specific_data['acc_time'])
-        print(self.curve_specific_data)
-        return True
+            return None
+        return data
 
     def trapezoid_vel(self, time_duration, current_position=(0.0, 0.0, 0.0)):
-        if time_duration >= self.time:
-            self.vel_curve = "default"
-            return (0.0, 0.0, 0.0)
-        omega = Utils.get_shortest_angle(math.atan2(self.y, self.x), current_position[2])
-        vel = self.curve_specific_data['vel'] # desired vel
-        if time_duration < self.curve_specific_data['acc_time']:
-            # accelerate
+        data = self.trajectory_data_list[self.trajectory_index]
+        if time_duration >= data['time']:
+            return self.default_vel(time_duration, current_position)
+        omega = Utils.get_shortest_angle(math.atan2(data['y'], data['x']),
+                                         current_position[2])
+        vel = data['vel'] # desired vel
+        if time_duration < data['acc_time']: # accelerate
             vel = self.max_acc * time_duration
-        elif time_duration > self.time - self.curve_specific_data['acc_time']:
-            # decelerate
-            vel -= self.max_acc * (time_duration - self.time + self.curve_specific_data['acc_time'])
-        return (vel * math.cos(omega), vel * math.sin(omega), self.theta/self.time)
+        elif time_duration > data['time'] - data['acc_time']: # decelerate
+            vel -= self.max_acc * (time_duration - data['time'] + data['acc_time'])
+        return (vel * math.cos(omega),
+                vel * math.sin(omega),
+                data['theta']/data['time'])
--- ros/src/waltzing_robot/waypoints.py
+++ ros/src/waltzing_robot/waypoints.py
@@ -18,27 +18,21 @@ class Waypoint(object):
             self.y = waypoint_dict.get('y', None)
             self.theta = waypoint_dict.get('theta', None)
             self.time = waypoint_dict.get('time', None)
+            self.control_points = waypoint_dict.get('control_points', None)
             self.vel_curve = waypoint_dict.get('vel_curve', default_vel_curve)
         else:
-            self.x, self.y, self.theta = None, None, None
+            self.x, self.y, self.theta, self.control_points = None, None, None, None
             self.time, self.vel_curve = None, default_vel_curve
         
     def __str__(self):
         string = ''
-        string += 'x: ' + str(self.x) + '\n'
-        string += 'y: ' + str(self.y) + '\n'
-        string += 'theta: ' + str(self.theta) + '\n'
+        string += 'x: ' + str(round(self.x, 3)) + '\n'
+        string += 'y: ' + str(round(self.y, 3)) + '\n'
+        string += 'theta: ' + str(round(self.theta, 3)) + '\n'
         string += 'time: ' + str(self.time) + '\n'
         string += 'vel_curve: ' + str(self.vel_curve) + '\n'
+        string += 'control_points: ' + str(self.control_points) + '\n'
         return string
-
-    def __sub__(self, other):
-        assert isinstance(other, Waypoint)
-        return {'x': self.x - other.x,
-                'y': self.y - other.y,
-                'theta': math.atan2(math.sin(self.theta - other.theta), math.cos(self.theta - other.theta)),
-                'time': self.time - other.time,
-                'vel_curve': self.vel_curve}
 
     def to_pose(self):
         """Return a Pose object representing waypoint
@@ -54,39 +48,43 @@ class Waypoints(object):
     dance for the robot
     
     :keyword arguments:
-        :waypoint_config: dict{'frame': string, 'waypoints': list}
-        :waypoints: list of dict{'x': float, 'y': float, 'theta': float, 'time':float}
-        :frame: string
-
+        :waypoint_config: dict{'default_vel_curve': string, 'waypoints': list}
+        :waypoints: list of dict of following format
+                    {'x': float,
+                     'y': float,
+                     'theta': float,
+                     'time':float,
+                     'vel_curve':string,
+                     'control_points':list of dict {'x':float, 'y':float}
+                    }
     """
 
     def __init__(self, **kwargs):
         if 'waypoint_config' in kwargs:
             waypoint_config = kwargs.get('waypoint_config')
-            self.frame = waypoint_config.get('frame', '')
-            self.default_vel_curve = waypoint_config.get('default_vel_curve', 'square')
+            self.default_vel_curve = waypoint_config.get('default_vel_curve', 'linear')
             waypoints = waypoint_config.get('waypoints', [])
         else:
-            self.frame = kwargs.get('frame', '')
-            self.default_vel_curve = kwargs.get('default_vel_curve', 'square')
+            self.default_vel_curve = kwargs.get('default_vel_curve', 'linear')
             waypoints = kwargs.get('waypoints', [])
         self.waypoints = [Waypoint(wp_dict, self.default_vel_curve) for wp_dict in waypoints]
 
     def __str__(self):
         string = ''
-        string += 'frame: ' + self.frame + '\n'
         string += 'waypoints:' + '\n'
         for wp in self.waypoints:
             string += '  - ' + str(wp).replace('\n', '\n    ')[:-4]
         return string
 
-    def to_pose_array(self):
+    def to_pose_array(self, frame):
         """Return a PoseArray object representing waypoints
+
+        :frame: string
         :returns: geometry_msgs.PoseArray
 
         """
         pose_array = PoseArray()
-        pose_array.header.frame_id = self.frame
+        pose_array.header.frame_id = frame
         pose_array.header.stamp = rospy.Time.now()
         pose_array.poses = [wp.to_pose() for wp in self.waypoints]
         return pose_array
